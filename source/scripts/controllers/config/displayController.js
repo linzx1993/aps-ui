2fbc86a40c7eeb7b845f989a9948eb21bea98600
linzx1993 <442765960@qq.com>
2018-02-24 09:27:00 +0800

    delete:只保留组件文件和配置文件

--- source/scripts/controllers/config/displayController.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by linzx on 2017/4/17.
- */
-'use strict';
-app.controller("displayController",["$rootScope","$scope","$timeout","tool","http","$state", function($rootScope,$scope,$timeout,tool,http,$state){
-    //点击显示项配置时，路由默认默认跳转显示第一个tab---工作区域（原一级页面）
-    $state.go('config.workArea');
-}]);
